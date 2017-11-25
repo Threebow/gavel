45bb58402f16540685c1c7984811613edc0e9c9d
Alex Stalmakov <threebowpr@gmail.com>
2017-11-25 12:55:05 -0800

    Update clearwarnings.js

--- commands/clearwarnings.js
+++ commands/clearwarnings.js
@@ -1,5 +1,3 @@
-const moment = require("moment");
-
 module.exports.run = (bot, message, args, con) => {
 	if(!bot.hasPermission(message, "MANAGE_MESSAGES")) return message.channel.send("You do not have permission to do this.");
 
